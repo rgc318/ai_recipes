d1c2708789f83a0484f9c6490dab989b82ae7d17
rgc318 <1511659008@11.com>
2025-07-24 23:33:57 +0800

    结构优化

--- app/core/exceptions/jwt_exceptions.py
+++ app/core/exceptions/jwt_exceptions.py
@@ -9,19 +9,19 @@ class UnauthorizedException(BaseBusinessException):
 class TokenExpiredException(UnauthorizedException):
     def __init__(self, message: str = None):
         super().__init__(message or ResponseCodeEnum.TOKEN_EXPIRED.message)
-        self.code = 401
+        self.code = ResponseCodeEnum.TOKEN_EXPIRED.code
 
 class InvalidTokenException(UnauthorizedException):
     def __init__(self, message: str = None):
         super().__init__(message or ResponseCodeEnum.TOKEN_INVALID.message)
-        self.code = 401
+        self.code = ResponseCodeEnum.TOKEN_INVALID.code
 
 class TokenRevokedException(UnauthorizedException):
     def __init__(self, message: str = None):
         super().__init__(message or ResponseCodeEnum.TOKEN_REVOKED.message)
-        self.code = 401
+        self.code = ResponseCodeEnum.TOKEN_REVOKED.code
 
 class TokenTypeMismatchException(UnauthorizedException):
     def __init__(self, message: str = None):
         super().__init__(message or ResponseCodeEnum.TOKEN_TYPE_MISMATCH.message)
-        self.code = 401
+        self.code = ResponseCodeEnum.TOKEN_TYPE_MISMATCH.code
